2800047dd645109e5f35c2c37f909845f3f1904f
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-12 03:23:36 -0700

    LOTS of Changes
    Added a lot of changes. File searching is done but a bit wonky. It _works_ but is not robust.

--- src/FindFile.java
+++ src/FindFile.java
@@ -30,32 +30,112 @@ import java.io.File;
  * */
 /****************************************************/
 public class FindFile {
-	
+
+	/*Instances Variables
+	 * maxfiles --> Maximum ## of files that may be found during the given search
+	 * matchedFilesCount --> Used to keep track of the current standing for the ## of files found
+	 * fileLocations --> Array of pathnames that the files were found in
+	 * targetFile --> The name of the file that is being searched for
+	 * */
 	private int maxFiles;
 	private int matchedFilesCount = 0;
 	private String[] fileLocations;
-	
+	private String targetFile;
+
 	public FindFile(int maxFiles){
 		this.maxFiles = maxFiles;
 		fileLocations = new String[maxFiles];
 	}
-	
-	
-	void directorySearch(String target, String dirName){
-		
+
+	/*	directorySearch(String,String)
+	 * This method, in conjunction with a helper method,
+	 * takes in two String values to represent a target
+	 * file name and a directory to start looking through.
+	 * A new File object is created and directory is validated
+	 * to be a directory using the isDirectory() method from 
+	 * the File class.
+	 * If it is a directory, send that directory to be looked
+	 * through in the lookForFile helper method (where the recursive lookup happens)
+	 * */
+	public void directorySearch(String target, String dirName){
+
+		File directory = new File(dirName);
+
+		setTargetFileName(target); //set the target name so I can use it later
+
+		if(directory.isDirectory()){
+			lookForFile(directory);
+		}else{
+			/* <p>Will throw an exception caught in the driving class
+			 * from returning a null directory. This could be permission
+			 * related and the user does not have access
+			 * to a particular directory</p>
+			 * 
+			 * @exception*/
+			throw new IllegalArgumentException();
+		}
 	}
 	
-	public String[] getFiles(){
-		
+	/* This method is where the recursive calling actually
+	 * happens. In this method, an object with the value
+	 * of a directory is passed in from the method directorySearch.
+	 * The directory is then checked here during each stack.
+	 * 
+	 * @param targetDirectory File Object type used to reference
+	 * the varying value of the current working directory.
+	 * 
+	 * @param currentPosition used as an Object pointer
+	 * to compare against the targetDirectory on the String level.
+	 * 
+	 * @param targetDirectory used to "drop down" a single dimensional
+	 * array of a list of Files in the current working directory
+	 * */
+	private void lookForFile(File targetDirectory) {
+
+		if (targetDirectory.isDirectory()) {
+			System.out.println("Looking for: " + getTargetFileName() 
+					+ " in: " + targetDirectory.getAbsoluteFile());
+
+			for (File currentPosition : targetDirectory.listFiles()) {
+				if (getTargetFileName().equalsIgnoreCase(currentPosition.getName())) {
+					fileLocations[matchedFilesCount++] = targetDirectory.getAbsolutePath();
+					System.out.println("Found " + targetFile + " in " + targetDirectory.getAbsolutePath());
+				} else {
+					/*Recursive call with */
+					lookForFile(currentPosition);
+				}
+			}
+		}
 	}
-	
+
+	public String[] getFileLocations(){
+		return fileLocations;
+	}
+
 	public int getCount(){
 		return matchedFilesCount;
 	}
-	
+
+	public String getTargetFileName() {
+		return targetFile;
+	}
+
+	public void setTargetFileName(String targetFile) {
+		this.targetFile = targetFile;
+	}
+
+	/*
+	 * Sets the maximum number of files 
+	 * to find that match the given
+	 * target name. Added some minor boundaries
+	 * on the possible number. 
+	 * 
+	 * @see public FindFile constructor
+	 * @see maxFiles
+	 * */
 	public void setMaxFiles(int maxFiles){
-		if(maxFiles <= 0){
-			System.out.println("invalid number: " + maxFiles);
+		if(maxFiles <= 0 || maxFiles >= 100000000){
+			System.out.println("invalid or really large number: " + maxFiles);
 		}else{
 			this.maxFiles = maxFiles;
 		}
--- src/SearchForFileDriver.java
+++ src/SearchForFileDriver.java
@@ -1,13 +1,45 @@
+import java.io.File;
+
+/* SearchForFileDriver class (base class)
+ *
+ * This class is the driver for the FindFile class
+ * the uses its main method to create a new object
+ * from that class with the String values
+ * of a file name and a directory.
+ * 
+ * @author Ian Bryan
+ * @version 1.0
+
+ * */
 
 public class SearchForFileDriver {
 
-	private String targetFile;
-	private String pathToSearch;
 	
 	public static void main(String[] args) {
-		
+		/*
+		 * @see maxFiles
+		 * */
 		FindFile finder = new FindFile(5);
-		finder.directorySearch("git", "C:/Users/Quigy Laptop/Desktop");
-	}
 
+		String targetFile = "FILE_NAME_HERE(INCLUDING_TYPE_EXTENSION)";
+		String pathToSearch = "STARTING_PATH_HERE";
+
+		/* try-catch block for catching
+		 * the exception that is thrown from
+		 * the FindFile class when the directory
+		 * is null. 
+		 * 
+		 * @throws
+		 * */
+		try{
+			finder.directorySearch(targetFile,pathToSearch);
+		}catch(IllegalArgumentException iae){
+			/*@exception 
+			 *@see FindFile
+			 *@see searchDirectory
+			 *@see else clause
+			 */
+			System.out.println(iae + " was thrown from the program and caught during runetime");
+		}
+	}
 }
